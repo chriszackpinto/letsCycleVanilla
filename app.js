2a5db4f88fab540c8f1d82aead9a6192afaeb8f3
zackendev <chrispinto2197@gmail.com>
2021-05-17 23:56:38 +0530

    Cleaning up commented bloat

--- app.js
+++ app.js
@@ -13,21 +13,6 @@ window.addEventListener("load", () => {
 
   const tides = document.querySelector(".tides");
 
-  let tide1 = document.getElementById("tide1");
-  let tide2 = document.getElementById("tide2");
-  let tide3 = document.getElementById("tide3");
-  let tide4 = document.getElementById("tide4");
-
-  let time1 = document.getElementById("time1");
-  let time2 = document.getElementById("time2");
-  let time3 = document.getElementById("time3");
-  let time4 = document.getElementById("time4");
-
-  let state1 = document.getElementById("state1");
-  let state2 = document.getElementById("state2");
-  let state3 = document.getElementById("state3");
-  let state4 = document.getElementById("state4");
-
   if (
     navigator.geolocation.getCurrentPosition((position) => {
       longi = position.coords.longitude;
@@ -91,27 +76,6 @@ window.addEventListener("load", () => {
           windComp.style.transform = `rotate(${windDegree}deg)`;
         });
 
-      // // tide
-      // fetch(
-      //   `https://tides.p.rapidapi.com/tides?latitude=${lati}&longitude=${longi}&duration=1440&interval=60`,
-      //   {
-      //     method: "GET",
-      //     headers: {
-      //       "x-rapidapi-key":
-      //         "0f1a47ebecmsh7b713ebb8402e84p1fdd6djsnc9b4d1524ce5",
-      //       "x-rapidapi-host": "tides.p.rapidapi.com",
-      //     },
-      //   }
-      // )
-      //   .then((response) => {
-      //     console.log(response);
-      //     const data = response.json();
-      //     console.log(data);
-      //   })
-      //   .catch((err) => {
-      //     console.error(err);
-      //   });
-
       // New API - stormglass (Public key on purpose)
 
       fetch(
@@ -140,63 +104,6 @@ window.addEventListener("load", () => {
             tides.insertAdjacentHTML(`beforeend`, html);
           });
         });
-      // .then((response1) => {
-      //     return response1.json();
-      //   })
-      //   .then((data1) => {
-      //     console.log(data1);
-      // .then((response) => {
-      //   const data = response.json();
-      //   console.log(data);
-      //   //     data.map((tide) => {
-      //   //       tides.innerHTML = ``;
-      //   //       const qTime = new Date(tide.time);
-      //   //       const html = ` <div class="card">
-      //   //       <h4 class="time">${qTime.getHours()}:${qTime.getMinutes()}</h4>
-      //   //       <h3 class="state">${tide.type}</h3>
-      //   //       <h4 class="height">${tide.height}</h4>
-      //   // </div>`;
-
-      //   //       tides.insertAdjacentHTML(`beforeend`, html);
-      //   //     });
-      // });
-
-      // Do something with response data.
-
-      // <div class="card first">
-      //     <h3 id="state1">Loading ... !</h3>
-      //     <h4 id="time1">Loading ... !</h4>
-      //   </div>
-      // fetch(
-      //   `https://tides.p.rapidapi.com/tides?latitude=${lati}&longitude=${longi}&duration=1440&interval=60`,
-      //   {
-      //     method: "GET",
-      //     headers: {
-      //       "x-rapidapi-key":
-      //         "0f1a47ebecmsh7b713ebb8402e84p1fdd6djsnc9b4d1524ce5",
-      //       "x-rapidapi-host": "tides.p.rapidapi.com",
-      //     },
-      //   }
-      // )
-      //   .then((response1) => {
-      //     return response1.json();
-      //   })
-      //   .then((data1) => {
-      //     console.log(data1);
-
-      //     time1.textContent = data1.extremes[0].datetime.slice(11, 19);
-      //     time2.textContent = data1.extremes[1].datetime.slice(11, 19);
-      //     time3.textContent = data1.extremes[2].datetime.slice(11, 19);
-
-      //     state1.textContent = data1.extremes[0].state;
-      //     state2.textContent = data1.extremes[1].state;
-      //     state3.textContent = data1.extremes[2].state;
-      //   })
-
-      //   .catch((err) => {
-      //     console.error(err);
-      //   });
     })
-    // EndIf
   );
 });
